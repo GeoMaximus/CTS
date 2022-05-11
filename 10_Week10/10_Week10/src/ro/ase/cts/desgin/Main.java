b6a13f3cb24abfc48d43b06de78d8fbad6b35e4c
Geo Maximus <pescarugeorgiana19@gmail.com>
2022-05-11 08:11:19 +0300

    chain of responsability
    Signed-off-by: Geo Maximus <pescarugeorgiana19@gmail.com>

---
+++ 10_Week10/10_Week10/src/ro/ase/cts/desgin/Main.java
@@ -0,0 +1,8 @@
+package ro.ase.cts.desgin;
+
+public class Main {
+    public static void main(String[] args) {
+        //CHAIN OF RESPONSIBILITY
+
+    }
+}
---
+++ 10_Week10/10_Week10/src/ro/ase/cts/design/chainofresponsability/Order.java
@@ -0,0 +1,4 @@
+package ro.ase.cts.design.chainofresponsability;
+
+public class Order {
+}
